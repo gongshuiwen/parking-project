bd1b51e8c662c36659a8f295450e2981cea7522a
gongshuiwen <shuiwen.gong@outlook.com>
2023-05-12 16:34:38 +0800

    opt(parking-common): 优化业务异常全局处理器

--- parking-common/src/main/java/org/example/parking/common/exception/GlobalBusinessExceptionHandler.java
+++ parking-common/src/main/java/org/example/parking/common/exception/GlobalBusinessExceptionHandler.java
@@ -2,7 +2,6 @@ package org.example.parking.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.parking.common.protocol.Result;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +15,9 @@ public class GlobalBusinessExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public Result<String> exceptionHandler(BusinessException e) {
-        log.error(e.getCode() + ": " + e.getMessage());
-        return Result.error(e.getCode(), e.getMessage());
+        Integer code = e.getCode();
+        String message = e.getMessage();
+        log.error("error code: " + code.toString() + " msg: " + message);
+        return Result.error(code, message);
     }
 }
